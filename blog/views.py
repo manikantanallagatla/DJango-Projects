4445db370e4ad6b84188cea691c764adc799919d
root <manikanta001nallagatlaresume@gmail.com>
2016-10-03 20:02:26 +0530

    Final blog but post_edit not working

--- blog/views.py
+++ blog/views.py
@@ -12,3 +12,20 @@ from django.shortcuts import get_object_or_404
 def post_detail(request,pk):
 	post = get_object_or_404(Post, pk = pk)
 	return render(request, 'blog/post_detail.html',{'post':post})
+
+#creating forms
+from django.shortcuts import redirect
+#with redirect
+from .forms import PostForm
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
